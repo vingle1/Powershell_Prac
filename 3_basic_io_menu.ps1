da22dd3e795fb0423e1da46c9e2b8303872ac888
Vaibhav Ingle <vaingle28@outlook.com>
2024-10-17 14:00:02 -0500

    updated menu to include IP and Hash information

--- 3_basic_io_menu.ps1
+++ 3_basic_io_menu.ps1
@@ -7,7 +7,7 @@ $name = Read-Host "Please enter your name"
 
 function Say-Hello {
     param($userName)
-    Write-Host "$greeting, $userName!"
+    Write-Host "$greeting, $userName!" -ForegroundColor Green
 
 }
 
@@ -42,28 +42,64 @@ function Show-UserInfo {
     }
 }
 
+function Show-IPInfo {
+    try{
+    $ipInfo = Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' }
+    if ($ipInfo){
+        Write-Host "IP Address Information:"
+        $ipInfo | ForEach-Object {
+            Write-Host "Interface: $($_.InterfaceAlias) - IP Address: $($_.IPAddress)"
+        }
+
+    } else {
+            Write-Host "Cannot find the IP address informationn"
+        }
+        }
+        catch {
+                Write-Host "Unable to get IP address information due to unknown error"
+        }
+}
+
+function show-FileHash {
+    $filePath = Read-Host "Specify the full file path"
+    if(Test-Path $filePath) {
+        $hash = Get-FileHash -Path $filePath
+        Write-Host "File: $($hash.Path)"
+        Write-Host "Hash Algorith: $($hash.Algorithm)"
+        Write-Host "Hash: $($hash.Hash)"
+    } 
+    else {
+        Write-Host "This file does not exist or the path you entered is incorrect" -ForegroundColor DarkRed
+    }
+}
+
 function Show-Menu {
 Clear-Host
+Say-Hello $name
 Write-Host "=========================="
 Write-Host " System Information Menu"
 Write-Host "=========================="
 Write-Host "1. Show Operating system Info"
 Write-Host "2. Show Powershell Version"
 Write-Host "3. Show User and Domain Information"
-Write-Host "4. Exit"
+Write-Host "4. Show all the IPV4 addresses associated with this computer"
+Write-Host "5. Show file hash"
+Write-Host "6. Exit"
 
 }
 
 do{
     Show-Menu
-    $choice = Read-Host "Please select an option (1-4)"
+    $choice = Read-Host "Please select an option (1-5)"
 
     switch ($choice){
         1 { Show-OSInfo }
         2 { Show-PSVersion }
         3 { Show-UserInfo }
-        4 { Write-Host "Exiting..."; break & exit }
-        default { Write-Host "Invalid choice." }
+        4 { Show-IPInfo }
+        5 { show-FileHash }
+        6 { Write-Host "Exiting..."; return <#exit#> } #instead of break use exit if not used within ISE
+        default { Write-Host "Invalid choice." -ForegroundColor Red }
 
     }
 
