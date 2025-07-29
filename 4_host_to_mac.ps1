2f335ebb7ee9b9abaa8bb83a7df7231cd6a5cca0
Vaibhav Ingle <vaingle28@outlook.com>
2025-07-29 09:18:39 -0400

    update ARP cache

--- 4_host_to_mac.ps1
+++ 4_host_to_mac.ps1
@@ -8,6 +8,9 @@ foreach ($FatboxName in $FatboxNames) {
     $DnsRecords = Resolve-DnsName $FatboxName -Type A
     $IPAddress = $DnsRecords | Select-Object -ExpandProperty IPAddress
 
+    # populate ARP cache
+    Test-Connection -ComputerName $IPAddress -Count 1 -ErrorAction SilentlyContinue | Out-Null
+
     # get MACs
     $Neighor = Get-NetNeighbor -IPAddress $IPAddress -ErrorAction SilentlyContinue
     $MacAddress = $Neighor.LinkLayerAddress
